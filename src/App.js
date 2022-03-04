db68ff79808cc7e24242504affd58f00ecacdc65
Pamela Szymaniec <pamelaszymaniec@MacBook-Bajki-Pro.local>
2022-03-04 14:47:05 +0100

    Events and prop drilling in action, delete feedback

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import FeedbackData from "./data/FeedbackData";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete feedback?"))
+      setFeedback(feedback.filter((item) => item.id !== id));
+  };
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackList feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,14 +1,13 @@
 import FeedbackItem from "./FeedbackItem";
 
-function FeedbackList({ feedback }) {
+function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
     return <p>No feedback yet.</p>;
   }
-  console.log(feedback.rating);
   return (
     <div className="feedback-list">
       {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} />
+        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
       ))}
     </div>
   );
